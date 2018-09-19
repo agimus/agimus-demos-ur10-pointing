865c7e4cc61d61f8ffce577ce4b4cd70bb167b0a
Joseph Mirabel <jmirabel@laas.fr>
2018-09-19 16:08:31 +0200

    [talos/manipulate_boxes] Add missing launch files + misc

--- talos/manipulate_boxes/supervisor.py
+++ talos/manipulate_boxes/supervisor.py
@@ -7,7 +7,12 @@ def hpTasks(sotrobot):
     com = COM ("talos", sotrobot)
     lf = Foot ("talos/leg_left_6_joint", sotrobot)
     rf = Foot ("talos/leg_right_6_joint", sotrobot)
-    return com + lf + rf
+    #return com + lf + rf
+    #return lf + rf
+    #return lf
+    #return rf
+    #return com
+    return Manifold()
 
 def makeSupervisorWithFactory (robot):
     from agimus_sot import Supervisor
@@ -42,14 +47,17 @@ def makeSupervisorWithFactory (robot):
     factory.setObjects (objects, handlesPerObjects, contactPerObjects)
     factory.environmentContacts (["table/top",])
 
-    factory.setRules (rules)
-    factory.setupFrames (srdf["grippers"], srdf["handles"], robot,)
+    #factory.setRules (rules)
+    factory.setupFrames (srdf["grippers"], srdf["handles"], robot,
+      )
+      #disabledGrippers=grippers)
       #disabledGrippers=["table/pose",])
     for gripper in grippers:
       for handle in handlesPerObjects[0]:
         factory.addAffordance (
           Affordance (gripper, handle,
-            openControlType="torque", closeControlType="torque",
+            #openControlType="torque", closeControlType="torque",
+            openControlType="position", closeControlType="position",
             refs = { "angle_open": (0,), "angle_close": (-0.5,), "torque": (-0.05,) },
             controlParams = { "torque_num": ( 5000., 1000.),
                 "torque_denom": (0.01,) },
