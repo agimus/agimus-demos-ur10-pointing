aed0070c991aa6919a8dfd249e805fea8340ae1b
Florent Lamiraux <florent@laas.fr>
2019-12-17 13:27:04 +0100

    [talos/manipulate_boxes] in plan_path.py, planPath is a function.

--- talos/manipulate_boxes/plan_path.py
+++ talos/manipulate_boxes/plan_path.py
@@ -1,10 +1,15 @@
-q1 = ps.configAtParam (0, ps.pathLength (1))
-q2 = q1 [::]
-rank = robot.rankInConfiguration["box/root_joint"]
-q2 [rank + 3 : rank + 7] = (
-    Quaternion([0, 1, 0, 0]) * Quaternion(q_init[rank + 3 : rank + 7])).\
-    toTuple()
-ps.resetGoalConfigs ()
-ps.setInitialConfig (q1)
-ps.addGoalConfig (q2)
-ps.setMaxIterPathPlanning (1000)
+from hpp import Quaternion
+# Plan a path to flip the box starting at the end of pathId.
+def planPath (ps, pathId = None):
+    if pathId is None: pathId = ps.numberPaths () - 1
+    q1 = ps.configAtParam (0, ps.pathLength (pathId))
+    q2 = q1 [::]
+    rank = ps.robot.rankInConfiguration["box/root_joint"]
+    q2 [rank + 3 : rank + 7] = (
+        Quaternion([0, 1, 0, 0]) * Quaternion(q1[rank + 3 : rank + 7])).\
+        toTuple()
+    ps.resetGoalConfigs ()
+    ps.setInitialConfig (q1)
+    ps.addGoalConfig (q2)
+    ps.setMaxIterPathPlanning (1000)
+    ps.solve ()
