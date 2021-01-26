435d89c79a41857bc439e3a0131266aa5167940d
Joseph Mirabel <jmirabel@laas.fr>
2021-01-26 10:30:34 +0100

    Add P72 model.

---
+++ srdf/P72.py
@@ -0,0 +1,12 @@
+
+handle_fmt="""
+<handle name="handle_{i}" clearance="0.01">
+    <position rpy="0 1.5707963267948966 0"/>
+    <link name="hole_{j:02}_link" />
+    <mask>1 1 1 1 1 1</mask>
+</handle>"""
+
+print("""<robot name="p72">""")
+for i in range(18):
+    print(handle_fmt.format(i=i, j=i+1))
+print("""</robot>""")
