59359c52d2c3971d28112f9458baa0976e6aa2a0
Joseph Mirabel <jmirabel@laas.fr>
2021-01-28 13:06:10 +0100

    [TSP] unit test.

--- tests/tsp.cpp
+++ tests/tsp.cpp
@@ -52,6 +52,73 @@ void test_neighborMatrix()
   }
 }
 
+void test_swap2opt()
+{
+  using tsp::path_t;
+  using tsp::approximative_kopt::swap2opt;
+  {
+    path_t path { 0, 1, 2, 3, 4 },
+           expp { 3, 2, 1, 0, 4 };
+    swap2opt(path, 0, 3);
+    CHECK_EQ(path, expp);
+  }
+  {
+    path_t path { 0, 1, 2, 3, 4 },
+           expp { 0, 1, 3, 2, 4 };
+    swap2opt(path, 2, 3);
+    CHECK_EQ(path, expp);
+  }
+}
+
+void test_swap3opt()
+{
+  using tsp::path_t;
+  using tsp::approximative_kopt::swap3opt;
+
+  int n = 7;
+  for (int i = 0; i < 5; ++i) {
+    tsp::distance_matrix_t d = tsp::randomDistanceMatrix(n);
+
+    path_t pathBefore { 1, 2, 3, 4, 5, 6 }, pathAfter (pathBefore);
+    double costBefore = tsp::evaluateCost(d, pathBefore);
+
+    CHECK_EQ(pathBefore.size(), n-1);
+    for (int i = 0; i < n-1; ++i) { // index of B
+      int A = (i==0 ? 0 : pathBefore[i-1]), B = pathBefore[i];
+      for (int j = i+2; j < n-1; ++j) { // index of D
+        int C = pathBefore[j-1], D = pathBefore[j];
+        for (int k = j+2; k < n; ++k) { // index of F
+          int E = pathBefore[k-1], F = (k==pathBefore.size() ? 0 : pathBefore[k]);
+
+          Eigen::Matrix<double, 8, 1> cases;
+          cases <<
+            d(A, B) + d(C, D) + d(E, F), // 0: initial
+            d(A, B) + d(C, E) + d(D, F), // 1: reverse DE
+            d(A, C) + d(B, D) + d(E, F), // 2: reverse BC
+            d(A, C) + d(B, E) + d(D, F), // 3: reverse BC, DE
+            d(A, D) + d(E, B) + d(C, F), // 4: exchange BC and DE
+            d(A, D) + d(E, C) + d(B, F), // 5: 
+            d(A, E) + d(D, B) + d(C, F), // 6: 
+            d(A, E) + d(D, C) + d(B, F); // 7: 
+          int minIndex;
+          cases.minCoeff(&minIndex);
+
+          bool res = swap3opt(d, pathAfter, i, j, k);
+
+          CHECK_EQ(res, minIndex>0);
+          
+          double costAfter = tsp::evaluateCost(d, pathAfter);
+          if (res) {
+            CHECK_LT(costAfter, costBefore);
+            pathAfter = pathBefore;
+          } else
+            CHECK_EQ(pathBefore, pathAfter);
+        }
+      }
+    }
+  }
+}
+
 void print(tsp::path_t p)
 {
   std::cout << "[ ";
@@ -78,6 +145,10 @@ void compare(const tsp::distance_matrix_t& d)
   std::cout << "=================================\n";
   measure(tsp::heuristic_nearest::solve, "HN ");
   //std::cout << "---------------------------------\n";
+  measure(tsp::approximative_kopt::solve2opt, "A2O");
+  //std::cout << "---------------------------------\n";
+  measure(tsp::approximative_kopt::solve3opt, "A3O");
+  //std::cout << "---------------------------------\n";
   measure(tsp::dynamic_programming::solveWithHeuristic, "DPH");
   //std::cout << "---------------------------------\n";
   measure(tsp::dynamic_programming::solve, "DP ");
@@ -92,6 +163,8 @@ void run_tests()
 {
   test_randomMatrix();
   test_neighborMatrix();
+  test_swap2opt();
+  test_swap3opt();
 }
 
 void run_benchs()
@@ -122,7 +195,7 @@ void run_benchs()
 int main()
 {
   run_tests();
-  //run_benchs();
+  run_benchs();
   return ::test::status;
 }
 
