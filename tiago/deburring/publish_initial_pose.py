ab4d374ff85abd9e3d12d3dc9f04f00eaea91a1a
Joseph Mirabel <jmirabel@laas.fr>
2021-02-08 15:31:35 +0100

    [tiago/deburring] Rigidly attach driller to hand in simulation

--- tiago/deburring/publish_initial_pose.py
+++ tiago/deburring/publish_initial_pose.py
@@ -3,39 +3,6 @@ import rospy, time
 from geometry_msgs.msg import PoseWithCovarianceStamped, Point, Quaternion
 from std_srvs.srv import Empty
 
-def attach_driller_to_tiago_gripper():
-    from gazebo_ros_link_attacher.srv import Attach, AttachRequest
-    from gazebo_msgs.srv import GetModelState, GetModelStateRequest
-
-    get_model_state = rospy.ServiceProxy("/gazebo/get_model_state", GetModelState)
-    get_model_state.wait_for_service()
-    def wait_for_model(name):
-        rospy.loginfo("Waiting for " + name + " model to be spawned.")
-        ok = False
-        req = GetModelStateRequest(name, '')
-        while not ok:
-            rsp = get_model_state(req)
-            ok = rsp.success
-            time.sleep(1)
-    wait_for_model("tiago")
-    wait_for_model("driller")
-
-    rospy.loginfo("Creating ServiceProxy to /link_attacher_node/attach")
-    attach_srv = rospy.ServiceProxy('/link_attacher_node/attach', Attach)
-    attach_srv.wait_for_service()
-    rospy.loginfo("Created ServiceProxy to /link_attacher_node/attach")
-
-    # Link them
-    req = AttachRequest()
-    req.model_name_1 = "tiago"
-    req.link_name_1 = "arm_7_link"
-    req.model_name_2 = "driller"
-    req.link_name_2 = "base_link"
-
-    res = attach_srv.call(req)
-    if not res.ok:
-        rospy.logerr("Could not attach driller to tiago hand.")
-
 def publish_initial_pose():
     rospy.wait_for_service("/global_localization")
 
@@ -74,5 +41,4 @@ def publish_initial_pose():
 
 rospy.init_node("publish_initial_pose", anonymous=True)
 
-attach_driller_to_tiago_gripper()
 publish_initial_pose()
