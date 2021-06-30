72fdbece3d22704269cb8e1456dda350365ef331
Florent Lamiraux <florent@laas.fr>
2021-06-30 14:23:19 +0000

    [ur10/pointing] Improve script_hpp.py

--- ur10/pointing/script_hpp.py
+++ ur10/pointing/script_hpp.py
@@ -5,7 +5,7 @@ from hpp.corbaserver.manipulation import Robot, loadServerPlugin, \
     createContext, newProblem, ProblemSolver, ConstraintGraph, \
     ConstraintGraphFactory, Rule, Constraints, CorbaClient
 from hpp.gepetto.manipulation import ViewerFactory
-from tools_hpp import ConfigGenerator
+from tools_hpp import ConfigGenerator, RosInterface
 
 class PartP72:
     urdfFilename = "package://agimus_demos/urdf/P72-with-table.urdf"
@@ -20,6 +20,25 @@ p.add_argument ('--context', type=str, metavar='context',
                 default=defaultContext,
                 help="identifier of ProblemSolver instance")
 args = p.parse_args ()
+
+joint_bounds = {}
+def setRobotJointBounds(which):
+    for jn, bound in jointBounds[which]:
+        robot.setJointBounds(jn, bound)
+
+def planMotionToHole(ri, q0, handle):
+    if ri is None:
+        ri = RosInterface(robot)
+    q_init = ri.getCurrentConfig(q0)
+    c = ConfigGenerator(graph)
+    res, qpg, qg = c.generateValidConfigForHandle(handle, q_init,
+                                                  qguesses = [q_init,])
+    if not res:
+        raise RuntimeError("Failed to generate valid config for hole {}".
+                           format(handle))
+    ps.setInitialConfig(q_init)
+    ps.addGoalConfig(qpg)
+    ps.solve()
 
 try:
     import rospy
@@ -43,8 +62,15 @@ def wd(o):
 client.manipulation.problem.selectProblem (args.context)
 
 robot = Robot("robot", "ur10", rootJointType="anchor", client=client)
+robot.opticalFrame = 'xtion_rgb_optical_frame'
 ps = ProblemSolver(robot)
 ps.loadPlugin("manipulation-spline-gradient-based.so")
+ps.addPathOptimizer("EnforceTransitionSemantic")
+ps.addPathOptimizer("SimpleTimeParameterization")
+# ps.selectConfigurationShooter('Gaussian')
+# ps.setParameter('ConfigurationShooter/Gaussian/center', 12*[0.] + [1.])
+# ps.setParameter('ConfigurationShooter/Gaussian/standardDeviation', 0.25)
+
 vf = ViewerFactory(ps)
 
 ## Shrink joint bounds of UR-10
@@ -54,6 +80,7 @@ jointBounds["default"] = [ (jn, robot.getJointBounds(jn)) \
                            if not jn.startswith('ur10/') else
                            (jn, [-pi, pi]) for jn in robot.jointNames]
 
+setRobotJointBounds("default")
 ## Remove some collision pairs
 #
 ur10JointNames = filter(lambda j: j.startswith("ur10/"), robot.jointNames)
@@ -84,3 +111,11 @@ factory.setGrippers(["ur10/gripper",])
 factory.setObjects(["part",], [part_handles], [[]])
 factory.generate()
 graph.initialize()
+# Set weights of levelset edges to 0
+for e in graph.edges.keys():
+    if e[-3:] == "_ls" and graph.getWeight(e) != -1:
+        graph.setWeight(e, 0)
+
+
+## Generate grasp configuration
+ri = None
--- ur10/pointing/tools_hpp.py
+++ ur10/pointing/tools_hpp.py
@@ -24,7 +24,15 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import rospy
+from pinocchio import XYZQUATToSE3, SE3ToXYZQUAT
+
 tool_gripper = 'ur10/gripper'
+rosNodeStarted = False
+
+def initRosNode():
+    if not rosNodeStarted:
+        rospy.init_node("hpp", disable_signals=True)
 
 class ConfigGenerator(object):
     def __init__(self, graph):
@@ -56,3 +64,62 @@ class ConfigGenerator(object):
             if res and self.robot.configIsValid(qres):
                 return True, qres
         return False, None
+
+class RosInterface(object):
+    nodeId = 0
+    def __init__(self, robot):
+        self.robot = robot
+        self.robotPrefix = robot.robotNames[0] + "/"
+        initRosNode()
+
+    def getCurrentConfig(self, q0, timeout=5.):
+        from sensor_msgs.msg import JointState
+        q = q0[:]
+        # Acquire robot state
+        msg = rospy.wait_for_message("/joint_states", JointState)
+        for ni, qi in zip(msg.name, msg.position):
+            jni = self.robotPrefix + ni
+            if self.robot.getJointConfigSize(jni) != 1:
+                continue
+            try:
+                rk = self.robot.rankInConfiguration[jni]
+            except KeyError:
+                continue
+            assert self.robot.getJointConfigSize(jni) == 1
+            q[rk] = qi
+
+        return q
+
+    def getObjectPose(self, q0, timeout=5.):
+        # the object should be placed wrt to the robot, as this is what the
+        # sensor tells us.
+        # Get pose of object wrt to the camera using TF
+        import tf2_ros, rospy
+
+        tfBuffer = tf2_ros.Buffer()
+        listener = tf2_ros.TransformListener(tfBuffer)
+
+        cameraFrame = self.robot.opticalFrame
+        qres = q0[:]
+
+        for obj in self.robot.robotNames[1:]:
+            objectFrame = obj + '/base_link_measured'
+            wMc = XYZQUATToSE3(self.robot.hppcorba.robot.getJointsPosition\
+                               (q0, [self.robotPrefix + cameraFrame])[0])
+            try:
+                _cMo = tfBuffer.lookup_transform(cameraFrame, objectFrame,
+                        rospy.Time(), rospy.Duration(timeout))
+                _cMo = _cMo.transform
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException,
+                    tf2_ros.ExtrapolationException) as e:
+                print('could not get TF transform : ', e)
+                raise RuntimeError(str(e))
+            cMo = XYZQUATToSE3([_cMo.translation.x, _cMo.translation.y,
+                                _cMo.translation.z, _cMo.rotation.x,
+                                _cMo.rotation.y, _cMo.rotation.z,
+                                _cMo.rotation.w])
+            rk = self.robot.rankInConfiguration[obj + '/root_joint']
+            assert self.robot.getJointConfigSize(obj + '/root_joint') == 7
+            qres[rk:rk+7] = SE3ToXYZQUAT (wMc * cMo)
+
+        return qres
