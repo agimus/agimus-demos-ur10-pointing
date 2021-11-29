2928bf2f65a5014e0df27ca289adf985c1f6d8a1
Florent Lamiraux <florent@laas.fr>
2021-11-29 07:57:00 +0100

    [ur10/docker] build image for ROS noetic in Ubuntu 20.04.

--- ur10/docker/build_image.sh
+++ ur10/docker/build_image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ros_distro=noetic
 address=gitlab.laas.fr:4567
 name=rob4fam/docker/ur10-pointing
 tag=4
@@ -9,4 +10,4 @@ if [ ! -d "rob4fam-models" ]; then
 else
   echo "rob4fam-models already checked out."
 fi
-docker build --tag $address/$name:$tag .
+docker build --tag $address/$name:$tag -f Dockerfile.$ros_distro .
