ee138ed86b39e7343bf22fbfb02d62d600e62dfe
Florent Lamiraux <florent@laas.fr>
2022-12-22 15:54:13 +0000

    [Calibration] Clean file.

--- calibration.py
+++ calibration.py
@@ -209,7 +209,7 @@ def readDataFromFigaroh(filename, q0):
             configurations.append(q)
     return configurations, wMcs
 
-def displayPose(nodeName, wMc):
+def displayPose(v, nodeName, wMc):
     if not nodeName in v.client.gui.getNodeList():
         v.client.gui.addXYZaxis(nodeName, [1,1,1,1], 0.015, 0.1)
     pose = list(wMc.translation)
@@ -219,45 +219,13 @@ def displayPose(nodeName, wMc):
 
 # Read configurations and cMo in files and display corresponding pose
 # in gepetto-gui
-def checkData(robot, v, input_directory, q_init, maxIndex):
-    with open('/tmp/robot.urdf', 'w') as f:
-        f.write(robot.urdfString)
-    pinRob = RobotWrapper()
-    pinRob.initFromURDF('/tmp/robot.urdf')
-    camera_frame_id = pinRob.model.getFrameId('camera_color_optical_frame')
-    count = 1
-    while count <= maxIndex:
-        poseFile = os.path.join(input_directory, f'pose_cPo_{count}.yaml')
-        configFile = os.path.join(input_directory, f'configuration_{count}')
-        try:
-            f1 = open(poseFile, 'r')
-            d = yaml.safe_load(f1)
-            f1.close()
-            f1 = open(configFile, 'r')
-            config = f1.readline()
-            f1.close()
-            cMo_tuple = list(zip(*d['data']))[0]
-            trans = np.array(cMo_tuple[:3])
-            rot = exp3(np.array(cMo_tuple[3:]))
-            cMo = SE3(translation = trans, rotation = rot)
-            listq = list(map(float, config.split(',')))
-            q = np.array(listq)
-            listq += q_init[6:]
-            forwardKinematics(pinRob.model, pinRob.data, q)
-            pinRob.framesForwardKinematics(q)
-            wMc = pinRob.data.oMf[camera_frame_id]
-            v.client.gui.setCameraTransform('scene_hpp_',
-                list(wMc.translation) +
-                list((Quaternion(wMc.rotation)*Quaternion(np.array([0,1,0,0]))).
-                     coeffs()))
-            wMo = wMc * cMo
-            displayPose('robot/chessboard', wMc)
-            v(listq)
-            print('Press enter')
-            input()
-        except FileNotFoundError as exc:
-            print(f'{poseFile} does not exist')
-        count+=1
+def checkData(robot, v, figaroh_csv_file, q_init):
+    configurations, wMcs = readDataFromFigaroh(figaroh_csv_file, q_init)
+    for q, wMc in zip(configurations, wMcs):
+        displayPose(v, 'robot/chessboard', wMc)
+        v(q)
+        print('Check pose of camera and press enter')
+        input()
 
 def computeCameraPose(mMe, eMc, eMc_measured):
     # we wish to compute a new position mMe_new of ref_camera_link in
